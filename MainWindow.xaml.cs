c55d2d15b0334d173b41e39f871409af5e29c719
Kari-The-Unicorn <inmatrixos@gmail.com>
2021-10-10 20:35:24 +0100

    Set up WPF project and added simple layout

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WPF_StudentsManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Student added", "Students Manager", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
