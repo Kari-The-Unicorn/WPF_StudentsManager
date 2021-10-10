6e256e3ad4436ea9d650fe9c9c0527f2e0f6b02e
Kari-The-Unicorn <inmatrixos@gmail.com>
2021-10-10 21:10:35 +0100

    updated layout plus actions on buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace WPF_StudentsManager
 {
@@ -23,7 +25,14 @@ namespace WPF_StudentsManager
 
         private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
         {
-
+            // Move students studentsListGrid to column 2 and back to 0 when clicked again
+            int column = (int)studentsListGrid.GetValue(Grid.ColumnProperty);
+            int newColumn = column == 0 ? 2 : 0;
+            studentsListGrid.SetValue(Grid.ColumnProperty, newColumn);
+            // change arrow icon when clicked to left and back to forward when clicked again (column grid = 0 then forward, column grid = 2 then left)
+            arrowMoveIcon.Source = newColumn == 0 ?
+                (System.Windows.Media.ImageSource)this.Resources["forward"] :
+                (System.Windows.Media.ImageSource)this.Resources["left"];
         }
     }
 }
