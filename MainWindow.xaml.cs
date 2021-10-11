9ba14c9a951876790b8d18474fc465cc488120a3
Kari-The-Unicorn <inmatrixos@gmail.com>
2021-10-11 21:31:22 +0100

    updated layout with comments

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,10 +25,20 @@ namespace WPF_StudentsManager
 
         private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
         {
+            //// Move students studentsListGrid to column 2 and back to 0 when clicked again
+            //int column = (int)studentsListGrid.GetValue(Grid.ColumnProperty);
+            //int newColumn = column == 0 ? 2 : 0;
+            //studentsListGrid.SetValue(Grid.ColumnProperty, newColumn);
+            //// change arrow icon when clicked to left and back to forward when clicked again (column grid = 0 then forward, column grid = 2 then left)
+            //arrowMoveIcon.Source = newColumn == 0 ?
+            //    (System.Windows.Media.ImageSource)this.Resources["forward"] :
+            //    (System.Windows.Media.ImageSource)this.Resources["left"];
+            // That can be also done as below:
+
             // Move students studentsListGrid to column 2 and back to 0 when clicked again
-            int column = (int)studentsListGrid.GetValue(Grid.ColumnProperty);
+            int column = Grid.GetColumn(studentsListGrid);
             int newColumn = column == 0 ? 2 : 0;
-            studentsListGrid.SetValue(Grid.ColumnProperty, newColumn);
+            Grid.SetColumn(studentsListGrid, newColumn);
             // change arrow icon when clicked to left and back to forward when clicked again (column grid = 0 then forward, column grid = 2 then left)
             arrowMoveIcon.Source = newColumn == 0 ?
                 (System.Windows.Media.ImageSource)this.Resources["forward"] :
