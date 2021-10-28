eda4ba56e2bd8a131d82c458d449ec53c2233717
Kari-The-Unicorn <inmatrixos@gmail.com>
2021-10-28 22:19:48 +0100

    updated layout with comments

--- Base/Observable.cs
+++ Base/Observable.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 namespace WPF_StudentsManager.Base
 {
     public class Observable : INotifyPropertyChanged
-        {
-            public event PropertyChangedEventHandler PropertyChanged;
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
 
-            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) // protected virtual - can be overwitten in subclass
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) // protected virtual - can be overwitten in subclass
         {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+    }
 }
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using WPF_StudentsManager.Base;
 using WPF_StudentsManager.DataProvider;
 using WPF_StudentsManager.Model;
 
 namespace WPF_StudentsManager.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : Observable
     {
         private IStudentsDataProvider _studentsDataProvider;
+        private Student _selectedStudent;
 
         public MainViewModel(IStudentsDataProvider studentsDataProvider)
         {
             _studentsDataProvider = studentsDataProvider;
             Students = new ObservableCollection<Student>();
         }
+
+        //propfull part 1
+        //private Student _selectedStudent;
+        //propfull part 2
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set
+            {
+                // if _selectedStudent is not equal to selected student that we have already
+                if (_selectedStudent != value)
+                {
+                    _selectedStudent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         public ObservableCollection<Student> Students { get; }
 
         // Load students to View Model
